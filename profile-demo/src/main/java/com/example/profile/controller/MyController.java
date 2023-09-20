7be4c41eb23c66a365a1c5f4bf838654e4c90132
Hamidul Islam <hamidulasm@gmail.com>
2023-09-20 13:15:07 +0530

    profile demo

---
+++ profile-demo/src/main/java/com/example/profile/controller/MyController.java
@@ -0,0 +1,30 @@
+package com.example.profile.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+	@Value("${spring.datasource.url}")
+	private String dataSourceUrl;
+
+	@Autowired
+	private Environment environment;
+
+	@GetMapping("/props")
+	public String sayWhichProps() {
+		System.out.println("sayWhichProps().........");
+		return dataSourceUrl;
+	}
+	
+	@GetMapping("/env")
+	public String env() {
+		System.out.println("env().........");
+		return environment.getProperty("spring.datasource.url");
+	}
+
+}
